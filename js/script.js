6acea73e38d2948668722e43408a54f9292f8524
Yabu920 <159709633+Yabu920@users.noreply.github.com>
2024-03-20 23:09:05 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemsUrl =
-        "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{cat_short_name}.json";
+        "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
 
